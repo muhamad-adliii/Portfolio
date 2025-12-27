b353b0139eff0ab832e87a60ac086770923bbdcd
muhamad adli <muhmdadli01@gmail.com>
2025-12-27 20:14:59 +0700

    v-1

---
+++ script.js
@@ -0,0 +1,12 @@
+const starsContainer = document.getElementById('starsContainer');
+        const numberOfStars = 150;
+
+        for (let i = 0; i < numberOfStars; i++) {
+            const star = document.createElement('div');
+            star.className = 'star';
+            star.style.left = Math.random() * 100 + '%';
+            star.style.top = Math.random() * 100 + '%';
+            star.style.animationDelay = Math.random() * 3 + 's';
+            star.style.animationDuration = (Math.random() * 2 + 2) + 's';
+            starsContainer.appendChild(star);
+        }
